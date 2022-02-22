68de04c96d933547df4228b3c8468216da53f5e3
nguyenchattrang <nguyenchattrang@gmail.com>
2022-02-22 23:14:41 +0700

    New category

--- tu code cho de xem/js/myjs.js
+++ tu code cho de xem/js/myjs.js
@@ -1,19 +1,15 @@
-// window.onscroll = function() {myFunction()};
-
-// // Get the navbar
-// var navbar = document.getElementById("navbar");
-
-
-// // Get the offset position of the navbar
-// var sticky = navbar.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     navbar.classList.add("sticky")
-//   } else {
-//     navbar.classList.remove("sticky");
-//   }
-// }
-
 feather.replace();
+
+function a(){
+    var inputLeft = document.getElementById("input-left");
+    
+    var a = document.getElementById("min");
+
+    a.innerHTML=inputLeft.value;
+
+}
+function b(){
+    var inputRight = document.getElementById("input-right");
+      var b = document.getElementById("max");
+    b.innerHTML=inputRight.value;
+}
